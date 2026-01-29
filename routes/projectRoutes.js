d9865572a4352ef29161c7f7552b41f4a49faba3
4drea4 <bakerand@outlook.com>
2026-01-29 15:21:49 -0600

    talked to the cable guy got distracted for a sec

---
+++ routes/projectRoutes.js
@@ -0,0 +1,20 @@
+const express = require('expresss');
+const router = express.Router();
+const Project = require('../../models/Project');
+const auth = require('../../utils/auth');
+
+//create a project post  /api projects
+router.post('/', auth, async (req,res)=> {
+    try{
+        const { name, description} = req.body;
+
+    if(!name) {
+        return res.status(400).json({message: 'You have to create a pproject name!'});
+    }
+    const project = await Project.create({
+      name,
+      description,
+      user: req.user._id  
+    });
+    }
+})
