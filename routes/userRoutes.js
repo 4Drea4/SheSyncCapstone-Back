99ddd510b67b5bc3ce714550660c000073863cf4
4drea4 <bakerand@outlook.com>
2026-01-29 14:21:34 -0600

    register route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const User = require('../models/User')
+const User = require('../models/User.js');
+
+//post api users register route
+router.post('/register' ,async (req,res) => {
+    try {
+        const {username, email, password} = req.body;
+
+        if(!username || !email || !password) {
+            return res.status(400).json({message: 'The username, email, or password is missing' });
+        }
+        const exists = await User.findOne({email});
+        if (exists) {
+            return res.status(400).json({message: 'Uh-oh this email address is already being used'});
+        }
+        const newUser = await User.create({username,email,password});
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({message: 'We could not register this user', error:error.message});
+    }
+});
