51f53a46453f562971a4f516dc61f77c688da22e
4drea4 <bakerand@outlook.com>
2026-01-29 13:34:50 -0600

    I wanted to extract just the bearer token but I cant figure it out

--- utils/auth.js
+++ utils/auth.js
@@ -1,1 +1,23 @@
 const jwt = require('jsonwebtoken');
+
+function auth(req,res, next){
+   let token = req.headers.authorization;
+
+   if(token) {
+    token = token.split(' ').pop().trim();
+   }
+   if (!token) {
+    return res.status(401).json(
+        {message: 'No token  found'}
+    );
+   }
+   try{ 
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.data ; //like username id and emails
+    return next();
+   } catch (error) {
+    return res.status(401).json({message: 'This token is not valid'});
+   }
+
+}
+module.exports = auth;
