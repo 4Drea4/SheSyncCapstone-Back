84a40d5c4b548a627e6df57351395262d67f0887
4drea4 <bakerand@outlook.com>
2026-01-29 10:50:40 -0600

    server is successfully running

--- server.js
+++ server.js
@@ -3,7 +3,20 @@ const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./config/connection');
 const app =express();
+const PORT =  process.env.PORT || 3001;
 
 //middleware
-app.use(cors({origin: process.env.CLIENT_ORIGIN })
-)
+app.use(cors({origin: process.env.CLIENT_ORIGIN }));
+app.use(express.json());
+
+//test to make sure its running
+app.get('/', (req,res)=> {
+    res.send('My api is running successfully She Sync is in full effect');
+});
+
+//database connection
+connectDB();
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`)
+})
