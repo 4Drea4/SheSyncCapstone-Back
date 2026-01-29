3f8faab82c400c6e7eed1dbc42f068b042bdab06
4drea4 <bakerand@outlook.com>
2026-01-29 13:44:47 -0600

    okay working out how to target the bearer token specifically instead of calling token and taking whatever follows

--- utils/auth.js
+++ utils/auth.js
@@ -1,20 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 function auth(req,res, next){
-   let token = req.headers.authorization;
+   const authHeader = req.headers.authorization;
 
-   if(token) {
-    token = token.split(' ').pop().trim();
-   }
-   if (!token) {
+   if(!authHeader)//takes the auth header and takes the bearer token specifically {
     return res.status(401).json(
         {message: 'No token  found'}
     );
    }
+   const token = authHeader.split(' ')[1];
+
    try{ 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.data ; //like username id and emails
-    return next();
+    next();
    } catch (error) {
     return res.status(401).json({message: 'This token is not valid'});
    }
