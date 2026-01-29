a4de4f6587d3dc8a2d2a5c8a95aea435a98ca42b
4drea4 <bakerand@outlook.com>
2026-01-29 13:53:59 -0600

    i found an example of the authheader putting it in my readme

--- utils/auth.js
+++ utils/auth.js
@@ -1,21 +1,22 @@
 const jwt = require('jsonwebtoken');
 
 function auth(req,res, next){
-   const authHeader = req.headers.authorization;
+   const authHeader = req.headers.authorization; //takes the auth header instead of variables
 
-   if(!authHeader)//takes the auth header and takes the bearer token specifically {
+   if(!authHeader)//middleware stop the response and give it an error.
+    {
     return res.status(401).json(
         {message: 'No token  found'}
     );
    }
-   const token = authHeader.split(' ')[1];
+   const token = authHeader.split(' ')[1]; //for the bearer token in the specific format
 
    try{ 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.data ; //like username id and emails
     next();
    } catch (error) {
-    return res.status(401).json({message: 'This token is not valid'});
+    return res.status(401).json({message: 'This token is not valid'}); //caught by middleware
    }
 
 }
