2f5bac490f190906a49d8cce6dcffcb570b43bfb
4drea4 <bakerand@outlook.com>
2026-01-29 14:28:53 -0600

    trying not to have the hashed password returned

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,15 +8,23 @@ router.post('/register' ,async (req,res) => {
     try {
         const {username, email, password} = req.body;
 
+        //validate info
         if(!username || !email || !password) {
             return res.status(400).json({message: 'The username, email, or password is missing' });
         }
+        //check to see if email already exists
         const exists = await User.findOne({email});
         if (exists) {
             return res.status(400).json({message: 'Uh-oh this email address is already being used'});
         }
+        //if not create a new user and hashbrown 
         const newUser = await User.create({username,email,password});
-        res.status(201).json(newUser);
+
+        //return the user but dont return the hashbrowned password
+        return res.status(201).json({
+            message: 'User'
+        };
+        message: 'This user was registered successfully'
     } catch (error) {
         res.status(400).json({message: 'We could not register this user', error:error.message});
     }
